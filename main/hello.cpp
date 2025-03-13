1581ea9af41d4b40df5994f83e11a989610f25db
Arfa <132257607+thearfa99@users.noreply.github.com>
2025-03-13 13:50:34 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,8 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I'm PES2UG22CS315\n";
-  cout << "This is a webhook tutorial 1\n";
-  cout << "This is a webhook tutorial 2\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
