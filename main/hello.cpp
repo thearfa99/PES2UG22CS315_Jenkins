fd01fabf36accadb684a2476b8d278cfbfe1a441
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2025-03-13 12:12:12 +0530

    Initial commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I'm PES2UG22CS315\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
